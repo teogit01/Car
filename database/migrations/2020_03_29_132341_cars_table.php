40165039d1c4e464ff925c59de85a4e98fb43b94
dckhoa203 <43290383+dckhoa203@users.noreply.github.com>
2020-03-29 20:30:58 +0700

    #khoa.do create migrate
    Tao bang csdl

---
+++ database/migrations/2020_03_29_132341_cars_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('cars')) {
+            Schema::create('cars', function (Blueprint $table) {
+                $table->increments('car_id')->comment('id');
+                $table->string('car_name')->comment('tên xe');
+                $table->text('car_description')->comment('mô tả khoa');
+
+                // log time
+                $table->timestamp('created_at')
+                    ->default(DB::raw('CURRENT_TIMESTAMP'))
+                    ->comment('ngày tạo');
+
+                $table->timestamp('updated_at')
+                    ->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+                    ->comment('ngày cập nhật');
+
+                $table->timestamp('deleted_at')
+                    ->nullable()
+                    ->comment('ngày xóa tạm');
+                // Setting unique
+                $table->unique(['academy_code', 'academy_name']);
+            });
+            DB::statement("ALTER TABLE `cars` comment 'Thông tin xe'");
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
