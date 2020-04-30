8bb89cad8ee03586746bb970d3ba174f913bf14e
dckhoa203 <43290383+dckhoa203@users.noreply.github.com>
2020-04-30 21:52:33 +0700

    CRUD Service, return data customer

--- resources/views/customer/layouts/popular.blade.php
+++ resources/views/customer/layouts/popular.blade.php
@@ -3,6 +3,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <!-- Section Tittle -->
+                @yield('popular-title')
                 <div class="section-tittle text-center mb-80">
                     <span>Most visited places</span>
                     <h2>Popular Locations</h2>
@@ -10,72 +11,8 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="single-location mb-30">
-                    <div class="location-img">
-                        <img src="{{asset('src/customer/img/gallery/location1.png')}}" alt="">
-                    </div>
-                    <div class="location-details">
-                        <p>New York</p>
-                        <a href="#" class="location-btn">65 <i class="ti-plus"></i> Location</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="single-location mb-30">
-                    <div class="location-img">
-                        <img src="{{asset('src/customer/img/gallery/location2.png')}}" alt="">
-                    </div>
-                    <div class="location-details">
-                        <p>Paris</p>
-                        <a href="#" class="location-btn">60 <i class="ti-plus"></i> Location</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="single-location mb-30">
-                    <div class="location-img">
-                        <img src="assets/img/gallery/location3.png" alt="">
-                    </div>
-                    <div class="location-details">
-                        <p>Rome</p>
-                        <a href="#" class="location-btn">50 <i class="ti-plus"></i> Location</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="single-location mb-30">
-                    <div class="location-img">
-                        <img src="assets/img/gallery/location4.png" alt="">
-                    </div>
-                    <div class="location-details">
-                        <p>Italy</p>
-                        <a href="#" class="location-btn">28 <i class="ti-plus"></i> Location</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="single-location mb-30">
-                    <div class="location-img">
-                        <img src="assets/img/gallery/location5.png" alt="">
-                    </div>
-                    <div class="location-details">
-                        <p>Nepal</p>
-                        <a href="#" class="location-btn">99 <i class="ti-plus"></i> Location</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="single-location mb-30">
-                    <div class="location-img">
-                        <img src="assets/img/gallery/location6.png" alt="">
-                    </div>
-                    <div class="location-details">
-                        <p>indonesia</p>
-                        <a href="#" class="location-btn">78 <i class="ti-plus"></i> Location</a>
-                    </div>
-                </div>
-            </div>
+            @yield('popular-content')
+            
         </div>
         <!-- More Btn -->
         <div class="row justify-content-center">
