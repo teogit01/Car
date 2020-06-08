580779add729f6d1ba6eb4aab7619ada5baaacb4
Khoa Do <khoadochi1998@gmail.com>
2020-06-08 15:28:01 +0700

    #khoa.do them seeder cho tai khoan admin

---
+++ database/seeds/AccountSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'username' => 'admin',
+            'password' => Hash::make('admin'),
+            'name' => 'Admin',
+            'role' => 'Admin',
+            'tel' => '0123456789',
+        ]);
+    }
+}
